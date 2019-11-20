35e0b928e3e8146e1ca6f13e7028f53583752065
masal9pse <returnymgstokh@icloud.com>
2019-11-20 16:31:45 +0900

    mountedをうまくvuexに反映できない。

--- src/store/index.js
+++ src/store/index.js
@@ -11,14 +11,72 @@ export default new Vuex.Store({
     todos: [],
     memo: []
   },
+  getters: {
+    getItemJson(state) {
+      state.todo = JSON.parse(localStorage.getItem("state.todos")) || [];
+    }
+  },
   mutations: {
-    toggleSideMenu(state) {
-      state.drawer = !state.drawer
+    // toggleSideMenu(state) {
+    //   state.drawer = !state.drawer
+    // },
+    templateJson(state) {
+      state.todos = JSON.parse(localStorage.getItem("state.todos")) || [];
+      let setJson = JSON.stringify(state.todos);
+      localStorage.setItem("state.todos", setJson);
+    },
+    addMemo() {
+      this.templateJson();
+      this.isActive = false;
+    },
+    addTodo(state) {
+      if (state.name != "") {
+        state.todos.push({
+          name: state.name,
+          count: state.count
+        });
+      }
+      state.name = "";
+      this.templateJson();
+    },
+    increment(state) {
+      state.count++;
+      this.templateJson();
+    },
+    decrement(state) {
+      if (state.count > 0) {
+        state.count--;
+      }
+      this.templateJson();
+    },
+    deleteItem(state, index) {
+      state.todos.splice(index, 1);
+      let setJson = JSON.stringify(state.todos);
+      localStorage.removeItem("state.todos");
+      localStorage.setItem("state.todos", setJson);
     }
   },
   actions: {
     toggleSideMenu({ commit }) {
       commit("toggleSideMenu")
-    }
-  },
+    },
+    templateJson({ commit }) {
+      commit("templateJson")
+    },
+    addMemo({ commit }) {
+      commit("addMemo")
+    },
+    addTodo({ commit }) {
+      commit("addTodo")
+    },
+    deleteItem({ commit }, index) {
+      commit("deleteItem", index)
+    },
+    increment({ commit }, state) {
+      commit("increment", state)
+    },
+    decrement({ commit }, state) {
+      commit("decrement", state)
+    },
+  }
 })
