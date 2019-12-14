4fea48d35d0e7f6b652756cbebaa4d57898a8e86
masato <51937772+masal9pse@users.noreply.github.com>
2019-12-15 01:44:48 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <v-app-bar app>
         <v-app-bar-nav-icon @click="toggleSideMenu"></v-app-bar-nav-icon>
         <v-toolbar-title class="headline text-uppercase">
-          <span class="font-weight-light">講義さぼりカウント</span>
+          <span class="font-weight-light">講義欠席回数カウント</span>
         </v-toolbar-title>
         <v-spacer></v-spacer>
       </v-app-bar>
