01c8a40d35d6412b9d822af90c0fa5a57a123d37
masato <51937772+masal9pse@users.noreply.github.com>
2019-12-16 13:25:20 +0900

    Update Count.vue

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -7,6 +7,18 @@
             <v-text-field v-model="name" label="授業名" @keyup.enter="addTodo" required></v-text-field>
           </v-col>
         </v-row>
+          <v-card card_id max-width="344" class="mx-auto">
+            <v-card-title>sample</v-card-title>
+            <v-card-text>
+              <v-text-field label="メモ" v-model="todo.memo" @input="addMemo"></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn @click="increment(todo)" color="primary">さぼり回数</v-btn>
+              <span>{{ todo.count }}</span>
+              <v-btn @click="decrement(todo)" color="error">間違い（－）</v-btn>
+              <v-btn @click="deleteItem(index)">削除</v-btn>
+            </v-card-actions>
+          </v-card>
         <div v-for="(todo,index) in ($store.state.todos)" :key="todo.name">
           <v-card card_id max-width="344" class="mx-auto">
             <v-card-title>{{todo.name}}</v-card-title>
